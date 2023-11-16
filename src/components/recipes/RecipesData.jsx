a9ffeb8ac53cc035a1db80d51859dbda397d3e15
FatmaSliti <fatmasliti289@gmail.com>
2023-11-16 20:20:19 +0100

    update

--- src/components/recipes/RecipesData.jsx
+++ src/components/recipes/RecipesData.jsx
@@ -4,28 +4,18 @@ import styles from './RecipesData.module.css'
 import { Link } from 'react-router-dom'
 import DetailsPage from "./DetailsPage";
 
-function RecipesData({ modalIsVisible }) {
-    // const [modalIsVisible, setModalIsVisible] = useState(true)
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
+
+function RecipesData({ modalIsVisible }) {
     const [data, setData] = useState([]);
     useState()
-    // const categories = ["chicken", "beef", "pasta", "fish", "vegetarian"];
 
     const [visibleMeals, setVisibleMeals] = useState(8);
 
-    // const showModalHandler = () => {
-    //     setModalIsVisible(true);
-    // }
-
-    // const hideModalHandler = () => {
-    //     setModalIsVisible(false);
-    // }
-
     useEffect(() => {
         const fetchMeals = async () => {
-            // const randomIndex = Math.floor(Math.random() * categories.length);
-            // const category = categories[randomIndex];
-
             const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s`);
             const data = await response.json();
             console.log(data);
@@ -52,10 +42,14 @@ function RecipesData({ modalIsVisible }) {
             <ul className={styles.list}>
                 {data.slice(0, visibleMeals).map((item) => (
                     <li key={item.id} className={styles.listItem}>
-                        <Link to={`/recipes/${item.id}`} style={{textDecoration: 'none'}}>
+                        <Link to={`/recipes/${item.id}`} style={{ textDecoration: 'none' }}>
                             <div onClick={modalIsVisible}>
                                 <h1 className={styles.title}>{item.title}</h1>
                                 <h2 className={styles.area}>{item.area}</h2>
+                                <div className={styles.icon}>
+                                    <p className={styles.showIngredients}>see more</p>
+                                    <FontAwesomeIcon icon={faArrowRight} beat />
+                                </div>
                             </div>
                         </Link>
                     </li>

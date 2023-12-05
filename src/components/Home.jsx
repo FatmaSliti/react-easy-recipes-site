706c59f21e0485bb58d8f9147a2f54a37b5cd8be
FatmaSliti <fatmasliti289@gmail.com>
2023-12-05 12:08:36 +0100

    remove the map

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -39,7 +39,6 @@ const Home = () => {
                         ))}
                     </ul>
                 </section>
-                <MapComponent />
             </main>
         </div >
     );
